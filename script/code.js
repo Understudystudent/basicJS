87267141521db823fa3dbf496c8362e0f62173f4
Understudystudent <carlmjames2@gmail.com>
2023-10-10 10:50:32 +0200

    Updated Codejs

--- script/code.js
+++ script/code.js
@@ -1,1 +1,5 @@
-consolelog
+let firstname = 'Carl'
+console.log("My name is Jay-donn" + firstname)
+console.log(firstname + " 200 brothers")
+console.log(`My name is + ${firstname}`)
+console.log(`${firstname} + 200 brothers`)
